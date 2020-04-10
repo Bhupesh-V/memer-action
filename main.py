f77a65c575616c9d1024eca9608020cc3047ff6e
Bhupesh-V <varshneybhupesh@gmail.com>
2020-04-10 18:53:13 +0530

    initial test -3

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ HOST_URL = "https://www.reddit.com/r/ProgrammerHumor.rss"
 
 def getMeme():
     memelist = []
-    print("Memes from ProgrammerHumor")
+    memedict = {}
     f = feedparser.parse(HOST_URL)
     for entry in f.entries:
         x = entry['content'][0]['value']
-        print("Title: " + entry["title"])
-        print("Post: " + str(entry["link"]))
         img = x[x.find("https://i.redd.it"): x.find("link") - 3]
-        print("Meme: " + img, end="\n")
-        memelist.append(img)
+        if img != "":
+            memedict["title"] = entry["title"]
+            memedict["src"] = str(entry["link"])
+            memedict["meme"] = img
+            memelist.append(memedict)
         random.shuffle(memelist)
         return memelist[0]
 
 
 def main():
     meme = getMeme()
-    print(f"::set-output name=meme::{meme}")
+    print(f"::set-output name=meme::{meme['meme']}")
+    print(f"::set-output name=title::{meme['title']}")
+    print(f"::set-output name=source::{meme['src']}")
 
 
 if __name__ == "__main__":
