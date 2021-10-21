54be9185c7c689c4fd0d20d3f4eea1e2ce6bf0d8
Bhupesh Varshney <varshneybhupesh@gmail.com>
2021-10-21 15:44:58 +0530

    test with no content type

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ FALLBACK = {
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
-    "Content-Type": "application/json",
 }
 
 
